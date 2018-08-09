06efc76b0a855666aac0de5e4a7e13e597b97149
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-08-09 13:08:36 -0400

    Clean up and some tests of map functions.

--- dataObjects/countyClass.js
+++ dataObjects/countyClass.js
@@ -1,13 +1,13 @@
 class County {
     constructor(countyName){
         this.name = countyName
+        //add this.polygon for map interaction?
         this.towns = {
         
         }
         this.bagDrops = Array(0)
         this.stats = {
         
-
         }
     }
 }
