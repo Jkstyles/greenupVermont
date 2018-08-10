93611e672f070a7ffa9a902e6827df49b2ddb952
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-08-10 15:41:09 -0400

    choropleth with data on hover

--- countyMap.js
+++ countyMap.js
@@ -1,11 +1,97 @@
-
-    var mymap = L.map('mapid').setView([44.0423, -72.6034], 8);
-    L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-	subdomains: 'abcd',
-	maxZoom: 19,
+var mymap = L.map('mapid').setView([44.0423, -72.6034], 8);
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    subdomains: 'abcd',
+    maxZoom: 19,
     id: 'mapbox.streets',
 }).addTo(mymap);
+let countyBoundaries
+function putBagNumbersInPolygonData() {
+    //inserts that data into the polygon objects.
+    let counties = countyPolygons.features
+    for (let countyIndex in counties) {
+        counties[countyIndex].properties.bagCount = bagCountOf(counties[countyIndex].properties.CNTYNAME.toString().toLowerCase())
+    }
+    countyBoundaries = L.geoJson(countyPolygons, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(mymap);
+}
+//toDo: make something like the above, but for teams.
 
-let countyBoundaries = L.geoJSON(countyPolygons);
-countyBoundaries.addTo(mymap)
+
+function bagCountOf(county) {
+    return vermont.counties[county].stats.bagCount
+}
+
+// each of the counties in countyBoundaries already has a bounding box, addisons is at the variable below.
+// this would make zooming easy when we get around to that.
+// mymap.fitBounds(countyBoundaries._layers['51']._bounds)
+
+function getColor(d) {
+    return d > 13 ? '#800026' :
+    d > 11 ? '#BD0026' :
+    d > 9 ? '#E31A1C' :
+    d > 7 ? '#FC4E2A' :
+    d > 5 ? '#FD8D3C' :
+    d > 3 ? '#FEB24C' :
+    d > 1 ? '#FED976' :
+    '#FFEDA0';
+}
+
+function style(feature) {
+    console.log(feature)
+    return {
+        fillColor: getColor(feature.properties.bagCount),
+        weight: 2,
+        opacity: 1,
+        color: 'black',
+        dashArray: '',
+        fillOpacity: 0.7
+    };
+}
+
+function highlightFeature(e) {
+    var layer = e.target;
+    
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+    
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+    info.update(layer.feature.properties)
+}
+
+function resetHighlight(e) {
+    countyBoundaries.resetStyle(e.target);
+    info.update()
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+    });
+}
+
+var info = L.control();
+
+info.onAdd = function (mymap) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Total Bags by County</h4>' +  (props ?
+    '<b>' + props.CNTYNAME + '</b><br />' + props.bagCount + ' bags'
+    : 'Hover over a state');
+};
+
+info.addTo(mymap);
