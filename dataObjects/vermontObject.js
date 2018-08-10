aba2e74e4a2826a21ed5d462be509c8945ccba47
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-08-10 11:32:36 -0400

    Added comments to explain the Vermont object better. Saved the teams that don't match to a town from get teams function to an array on the state object.

--- dataObjects/vermontObject.js
+++ dataObjects/vermontObject.js
@@ -26,19 +26,22 @@ class Vermont {
         .on('value', (snapshot) => {
             let trashDropsObject = snapshot.val()
             console.log('data retrieved')
+            // create the arrays to store bagDrops in each county
             for (let county in this.counties) {
                 this.counties[county].bagDrops = Array(0)
             }
+            //for each trashdrop
             for (var key in trashDropsObject) {
                 let countyBoundaries = L.geoJSON(countyPolygons);
                 // put the coordinates of the trash drop object into a conveinient form
                 let keyCoordinates = [trashDropsObject[key].location.longitude, trashDropsObject[key].location.latitude]
-                // figure out which county the point is in, then cleans up that county name so it's usable in the next function
+                // figure out which county the drop is in, then cleans up that county name so it's usable in the next function
                 var resultsArray = leafletPip.pointInLayer(keyCoordinates, countyBoundaries, true)
                 var results = resultsArray[0].feature.properties.CNTYNAME.toLowerCase()
-                //looks through the list of counties in our vermont object, finds the one whose name matches the one the trash drop is in. Then pushes the trash drop into an array in that county object.
+                //looks through the list of counties in our vermont object, finds the one whose name matches the one the trash drop is in.
                 for(let county in this.counties){
                     if(results === this.counties[county].name){
+                        //Then push the trash drop into the array in that county object we set up earlier.
                         this.counties[county].bagDrops.push(trashDropsObject[key])
                     }            
                 }
@@ -68,23 +71,31 @@ class Vermont {
     }
     
     getBagStats() {
+        //set up an array to work with at the state level later.
         let stateBagCountArray = []
+        //For each county
         for (let county in this.counties) {
+            //set up a working array
             let bagCountArray = []
+            //Take each drop in the array, and push its number of bags to the county's array
             for(let bagDrop in this.counties[county].bagDrops){
                 let bagNumber = this.counties[county].bagDrops[bagDrop].bagCount
                 bagCountArray.push(bagNumber)
             }
-            
+            // add up the bag numbers in the county array for a total number of bags in the county
             totalBagsDropped = bagCountArray.reduce(function (total, currentValue) {
                 return total + currentValue;
             }, 0);
+            // 'Save' the total number of bags at the county level to a property of the County Object.
             this.counties[county].stats.bagCount = totalBagsDropped
+            // take that total number of county bags and push it to the state level array.
             stateBagCountArray.push(totalBagsDropped)
         }
+        // sum up bag counts again. this time at the state level to get a total number of bags in the state.
         let stateBagCount = stateBagCountArray.reduce(function (total, currentValue) {
             return total + currentValue;
         }, 0);
+        // 'Save' the total number of bags at the state level to a property of the Vermont Object.
         this.stats.bagCount = stateBagCount
     }
     
@@ -93,7 +104,7 @@ class Vermont {
         .database()
         .ref('teams/')
         .on('value', (snapshot) => {
-            let TownlessTeamsArray = []
+            this.townlessTeamsArray = []
             for (let county in this.counties) {
                 this.counties[county].teams = Array(0)
             }
@@ -113,7 +124,7 @@ class Vermont {
                     if (countyObject){
                         countyObject.teams.push(teamCountObject[team])
                     } else {
-                        TownlessTeamsArray.push(teamCountObject[team])
+                        this.townlessTeamsArray.push(teamCountObject[team])
                     }
                 }
             }
@@ -124,16 +135,20 @@ class Vermont {
             this.getTotalTeams()
         })
     }
+
+    //Gets the team count from each county and sums it to get the total teams in the state.
     getTotalTeams() {
         let teamCountArray = []
         for (let county in this.counties) {
             teamCountArray.push(this.counties[county].stats.totalTeams)
         } 
-        totalTeams = teamCountArray.reduce(function (total, currentValue) {
+        let totalCountyTeams = teamCountArray.reduce(function (total, currentValue) {
             return total + currentValue;
         }, 0);
-        this.stats.totalTeams = totalTeams
+        this.stats.totalTeams = totalCountyTeams + this.townlessTeamsArray.length
     }
+    //Apparently each county in vermont has a number that refers just to that county.
+    //This is a function that takes the County Number, listed for each town in our town polygons, and returns the county object that number corresponds to.
     countyNumber(CNTYNumber) {
         switch (CNTYNumber) {
             case 19:
