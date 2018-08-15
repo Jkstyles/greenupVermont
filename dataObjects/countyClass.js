40f0f643f3a03b34432b2326c5e367383b363466
deadion2ttip <39771562+deadion2ttip@users.noreply.github.com>
2018-08-14 20:08:36 -0400

    Simplifying vermont object and slight refactoring

--- dataObjects/countyClass.js
+++ dataObjects/countyClass.js
@@ -1,52 +1,53 @@
 class County {
     constructor(countyName){
         this.name = countyName
-        //add this.polygon for map interaction?
+        //ToDo?:add this.polygon for map interaction?
         this.towns = {
             
         }
+        this.teams = Array(0)
         this.bagDrops = Array(0)
         this.stats = {
             
         }
     }
     getBagStats() {
-        //set up an array to work with at the state level later.
+        //set up an array to work with at the county level later.
         let countyBagCountArray = []
-        //For each county
+        //For each town
         for (let town in this.towns) {
             //set up a working array
             let townBagCountArray = []
-            //Take each drop in the array, and push its number of bags to the county's array
+            //Take each drop in the array, and push its number of bags to the towns's array
             for(let bagDrop in this.towns[town].bagDrops){
                 let bagNumber = this.towns[town].bagDrops[bagDrop].bagCount
                 townBagCountArray.push(bagNumber)
             }
-            // add up the bag numbers in the county array for a total number of bags in the county
-            totalBagsDropped = townBagCountArray.reduce(function (total, currentValue) {
-                return total + currentValue;
-            }, 0);
-            // 'Save' the total number of bags at the county level to a property of the County Object.
+            // add up the bag numbers in the town array for a total number of bags in the town
+            totalBagsDropped = _.sum(townBagCountArray)
+            // 'Save' the total number of bags at the town level to a property of the town Object.
             this.towns[town].stats.bagCount = totalBagsDropped
-            // take that total number of county bags and push it to the state level array.
+            // take that total number of town bags and push it to the county level array.
             countyBagCountArray.push(totalBagsDropped)
         }
         // sum up bag counts again. this time at the county level to get a total number of bags in the county.
-        let countyBagCount = countyBagCountArray.reduce(function (total, currentValue) {
-            return total + currentValue;
-        }, 0);
-        // 'Save' the total number of bags at the state level to a property of the Vermont Object.
+        let countyBagCount = _.sum(countyBagCountArray)
+        // 'Save' the total number of bags at the county level to a property of the county Object.
         this.stats.bagCount = countyBagCount
     }
     getTeamStats() {
+        //set up an array to work with later
         let teamCountArray = []
+        //for each town
         for (let town in this.towns) {
+            //set the totalTeams subproperty of stats to the number of towns
             this.towns[town].stats.totalTeams = this.towns[town].teams.length
+            //and push that number of towns to our working array
             teamCountArray.push(this.towns[town].stats.totalTeams)
         }
-        let totalCountyTeams = teamCountArray.reduce(function (total, currentValue) {
-            return total + currentValue;
-        }, 0);
+        //add up the tnumber of teams in each town
+        let totalCountyTeams = _.sum(teamCountArray)
+        //and 'save' that number as a property of the county
         this.stats.totalTeams = totalCountyTeams
     }
 }
