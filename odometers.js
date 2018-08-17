3ca4e8399e6f3109a7b9b50db95acc96d03b76ad
Sean Stone <smstone@uvm.edu>
2018-08-17 08:04:08 -0400

    it said i have to commit

--- odometers.js
+++ odometers.js
@@ -1,13 +1,23 @@
+// (1).pad(3) // => "001"
+// (10).pad(3) // => "010"
+// (100).pad(3) // => "100"
+  
 
 function updateOdometer() {
+
+    Number.prototype.pad = function(size) {
+        var s = String(this);
+        while (s.length < (size || 2)) {s = "0" + s;}
+        return s;
+      }
  
     if (level === 'state') {
  
         setTimeout(function () {
-            bagsOdometer.innerHTML = vermont.stats.bagCount;
+            bagsOdometer.innerHTML = (vermont.stats.bagCount)
         }, 200);
         setTimeout(function () {
-            teamsOdometer.innerHTML = vermont.stats.totalTeams;
+            teamsOdometer.innerHTML = (vermont.stats.totalTeams)
         }, 400);
         setTimeout(function () {
             usersOdometer.innerHTML = vermont.stats.totalUsers;
